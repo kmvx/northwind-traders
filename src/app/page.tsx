208b6c71e518a9b3bd81a9b3e1eebe16ff59ebe2
Kolya Kosenko <kansev4k@gmail.com>
2025-09-29 22:41:45 +0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Image from 'next/image';
+import { ExternalLinkIcon } from 'lucide-react';
+
+const About: React.FC = () => {
+  return (
+    <div className="flex flex-col xl:flex-row h-full bg-blue-950 text-[max(1.5rem,1vw)]">
+      <div className="w-full xl:w-3/5">
+        <Image
+          alt=""
+          src="https://images.unsplash.com/photo-1508185066415-13f4acf9b564?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1959&q=80"
+          className="w-full h-full object-cover"
+          width="1959"
+          height="1073"
+        />
+      </div>
+      <div className="flex flex-col justify-center text-white xl:w-2/5 sm:p-[2vw]">
+        <h1 className="m-2 my-5 text-[max(3rem,3vw)] tracking-widest">
+          North&shy;wind Traders
+        </h1>
+        <div className="m-2 text-gray-300">
+          <p>
+            <span>This is a demo web application client for </span>
+            <a
+              href="https://en.wikiversity.org/wiki/Database_Examples/Northwind"
+              target="_blank"
+              rel="noreferrer"
+              className="text-blue-400 inline-flex items-center gap-1"
+            >
+              Northwind database <ExternalLinkIcon size={16} />
+            </a>
+            .
+          </p>
+          <p>
+            See{' '}
+            <a
+              href="https://kmvx.pages.dev/projects/northwind-traders"
+              target="_blank"
+              rel="noreferrer"
+              className="text-blue-400 inline-flex items-center gap-1"
+            >
+              project description <ExternalLinkIcon size={16} />
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
