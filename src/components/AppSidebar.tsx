3d8c842e720dbd624176abc082c7c4b45a4f693d
Kolya Kosenko <kansev4k@gmail.com>
2025-10-01 16:49:34 +0300

    Add sidebar

---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,58 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar';
+import { HouseIcon, LucideIcon } from 'lucide-react';
+
+interface SidebarItem {
+  title: string;
+  url: string;
+  icon: LucideIcon;
+}
+
+const items: SidebarItem[] = [
+  {
+    title: 'About',
+    url: '#',
+    icon: HouseIcon,
+  },
+  {
+    title: 'Home',
+    url: '#',
+    icon: HouseIcon,
+  },
+];
+
+export default function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+}
