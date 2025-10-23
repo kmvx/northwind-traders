03ed70ce3304c800a495c0d52c9f29fd88d31cc4
Kolya Kosenko <kansev4k@gmail.com>
2025-10-23 11:32:20 +0300

    Simplify grid in property grid component

--- src/ui/PropertyGrid.tsx
+++ src/ui/PropertyGrid.tsx
@@ -1,29 +1,35 @@
-import { Fragment } from 'react';
-
-type ProPropertyGridItemType = {
+type PropertyGridItemType = {
   name: string;
   description?: string;
   value: React.ReactNode;
 };
 
 interface PropertyGridProps {
-  items: ProPropertyGridItemType[];
+  items: PropertyGridItemType[];
 }
 
 export default function PropertyGrid({ items }: PropertyGridProps) {
   return (
-    <div className="grid grid-cols-2 2xl:grid-cols-4 items-center gap-y-2 gap-x-8">
-      {items.map(({ name, description, value }) => (
-        <Fragment key={name}>
-          <div>
-            <div>{name}:</div>
-            <div className="text-xs text-muted-foreground indent-4">
-              {description}
-            </div>
-          </div>
-          <b className="text-end">{value}</b>
-        </Fragment>
+    <div className="grid grid-cols-1 2xl:grid-cols-2 items-center gap-y-2 gap-x-8">
+      {items.map((item) => (
+        <Item item={item} key={item.name} />
       ))}
+    </div>
+  );
+}
+
+function Item({ item }: { item: PropertyGridItemType }) {
+  return (
+    <div className="flex flex-col sm:flex-row flex-wrap gap-4">
+      <div>
+        <div>{item.name}:</div>
+        {item.description && (
+          <div className="text-xs text-muted-foreground indent-4">
+            {item.description}
+          </div>
+        )}
+      </div>
+      <b className="text-end grow">{item.value}</b>
     </div>
   );
 }
