76549a3fff0267310e2a2371fa9df894127e6e97
Kolya Kosenko <kansev4k@gmail.com>
2025-10-30 10:07:02 +0200

    Improve sidebar on low height screens

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -89,12 +89,7 @@ export default function AppSidebar() {
           paddingTop: 'env(safe-area-inset-top)',
         }}
       >
-        <Link
-          href="/"
-          className="text-4xl font-serif italic font-black drop-shadow-lg bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-teal-600 dark:from-blue-300 dark:to-teal-300"
-        >
-          Northwind Traders
-        </Link>
+        <Logo />
         <Separator />
       </SidebarHeader>
       <SidebarContent className="h-max-sm:gap-0">
@@ -131,8 +126,22 @@ export default function AppSidebar() {
             </div>
           </SidebarGroupContent>
         </SidebarGroup>
+        <SidebarGroup className="hidden h-max-sm:flex">
+          <Logo />
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
+  );
+}
+
+function Logo() {
+  return (
+    <Link
+      href="/"
+      className="text-4xl font-serif italic font-black drop-shadow-lg bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-teal-600 dark:from-blue-300 dark:to-teal-300"
+    >
+      Northwind Traders
+    </Link>
   );
 }
