b7bf1689c1cc6f0e0ad547250497e0773585ca00
Kolya Kosenko <kansev4k@gmail.com>
2025-10-31 09:47:10 +0200

    Extract icon component in the ContactPhone component (Refactor)

--- src/entities/shared/ContactPhone.tsx
+++ src/entities/shared/ContactPhone.tsx
@@ -11,13 +11,11 @@ export default function ContactPhone({
   description?: string;
   isFax?: boolean;
 }) {
+  const IconComponent = isFax ? PrinterIcon : PhoneIcon;
+
   return (
     <div className="flex items-center gap-2" title={isFax ? 'Fax' : 'Phone'}>
-      {isFax ? (
-        <PrinterIcon className="min-w-4 size-4 text-muted-foreground" />
-      ) : (
-        <PhoneIcon className="min-w-4 size-4 text-muted-foreground" />
-      )}
+      <IconComponent className="min-w-4 size-4 text-muted-foreground" />
       <span className="flex items-center gap-2">
         <b>{phone}</b>
         <CopyButton content={phone} />
