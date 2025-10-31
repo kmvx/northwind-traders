41cf973bdd20f8a610e01b9c61362e5308c722ad
Kolya Kosenko <kansev4k@gmail.com>
2025-10-31 08:19:34 +0200

    Use property grin in the Supplier page

--- src/entities/suppliers/Supplier.tsx
+++ src/entities/suppliers/Supplier.tsx
@@ -3,7 +3,13 @@
 import React from 'react';
 
 import { useQuerySupplier } from '@/net';
-import { ErrorMessage, PanelCentred, Typography, WaitSpinner } from '@/ui';
+import {
+  ErrorMessage,
+  PanelCentred,
+  PropertyGrid,
+  Typography,
+  WaitSpinner,
+} from '@/ui';
 import { joinFields, setDocumentTitle } from '@/utils';
 
 import { ContactAddress, ContactPerson, ContactPhone } from '../shared';
@@ -21,33 +27,47 @@ const Supplier: React.FC<SupplierProps> = ({ id }) => {
   if (isLoading) return <WaitSpinner />;
   if (!data) return <div>No data</div>;
 
+  const items = [
+    {
+      name: 'Address',
+      value: (
+        <ContactAddress
+          country={data.country}
+          address={joinFields(
+            data.country,
+            data.region,
+            data.city,
+            data.address,
+            data.postalCode,
+          )}
+        />
+      ),
+      description: 'Supplier business street address.',
+    },
+    {
+      name: 'Contact name and title',
+      value: (
+        <ContactPerson
+          name={data.contactName}
+          contactTitle={data.contactTitle}
+        />
+      ),
+      description:
+        'The name and job title of the primary contact person for the supplier.',
+    },
+    {
+      name: 'Phone',
+      value: <ContactPhone phone={data.phone} />,
+      description: 'The supplier contact phone number.',
+    },
+  ];
+
   return (
     <PanelCentred className="flex flex-col gap-4">
       <Typography.Header1>{data.companyName}</Typography.Header1>
       <div className="flex flex-col gap-4">
         <div className="text-center">Supplier company</div>
-
-        <div className="flex flex-col md:flex-row flex-wrap gap-2 gap-x-4">
-          <div className="flex flex-col gap-4">
-            <ContactAddress
-              country={data.country}
-              address={joinFields(
-                data.country,
-                data.region,
-                data.city,
-                data.address,
-                data.postalCode,
-              )}
-            />
-
-            <ContactPhone phone={data.phone} />
-          </div>
-
-          <ContactPerson
-            name={data.contactName}
-            contactTitle={data.contactTitle}
-          />
-        </div>
+        <PropertyGrid items={items} />
       </div>
     </PanelCentred>
   );
