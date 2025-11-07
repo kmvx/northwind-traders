fc7de55080f82e87ad30424954b081d90b8d1309
Kolya Kosenko <kansev4k@gmail.com>
2025-11-07 19:36:17 +0200

    Add orders and suppliers charts

--- src/features/charts/Charts.tsx
+++ src/features/charts/Charts.tsx
@@ -1,38 +1,108 @@
 'use client';
 
-import { CustomersBarChart } from '@/features/charts/BarChart';
-import { CustomersWorldMapChart } from '@/features/charts/WorldMapChart';
-import { useQueryCustomers } from '@/net';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import {
+  CustomersBarChart,
+  OrdersBarChart,
+  SuppliersBarChart,
+} from '@/features/charts/BarChart';
+import {
+  CustomersWorldMapChart,
+  OrdersWorldMapChart,
+  SuppliersWorldMapChart,
+} from '@/features/charts/WorldMapChart';
+import { useQueryCustomers, useQueryOrders, useQuerySuppliers } from '@/net';
 import { PanelCentred } from '@/ui';
 
-import HeaderChart from './HeaderChart';
-
 const Charts: React.FC = () => {
-  // Network data
-  const {
-    data: dataCustomers,
-    error: errorCustomers,
-    isLoading: isLoadingCustomers,
-    refetch: refetchCustomers,
-  } = useQueryCustomers();
+  const tabInfos = [
+    { label: 'Orders', content: <OrdersCharts /> },
+    { label: 'Customers', content: <CustomersCharts /> },
+    { label: 'Suppliers', content: <SuppliersCharts /> },
+  ].map((info) => ({ ...info, name: info.label.toLowerCase() }));
 
   return (
-    <PanelCentred className="flex flex-col items-center gap-4">
-      <HeaderChart name="customers" />
-      <div className="flex flex-wrap gap-2">
-        <CustomersWorldMapChart
-          countriesQueryResult={{
-            countries: dataCustomers?.map((item) => item.country),
-            error: errorCustomers,
-            isLoading: isLoadingCustomers,
-            refetch: refetchCustomers,
-          }}
-          hue={30}
-          allowZoom
-        />
-        <CustomersBarChart />
-      </div>
+    <PanelCentred>
+      <Tabs defaultValue={tabInfos[0].name}>
+        <TabsList>
+          {tabInfos.map((info) => (
+            <TabsTrigger value={info.name} key={info.name}>
+              {info.label}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+        {tabInfos.map((info) => (
+          <TabsContent value={info.name} key={info.name}>
+            <div className="flex flex-col gap-4">
+              <h3 className="text-center text-2xl">
+                Distribution of count of <b>{info.name}</b> by countries
+              </h3>
+              {info.content}
+            </div>
+          </TabsContent>
+        ))}
+      </Tabs>
     </PanelCentred>
+  );
+};
+
+const CustomersCharts: React.FC = () => {
+  const { data, error, isLoading, refetch } = useQueryCustomers();
+
+  return (
+    <div className="flex flex-wrap gap-2">
+      <CustomersWorldMapChart
+        countriesQueryResult={{
+          countries: data?.map((item) => item.country),
+          error: error,
+          isLoading: isLoading,
+          refetch: refetch,
+        }}
+        hue={30}
+        allowZoom
+      />
+      <CustomersBarChart />
+    </div>
+  );
+};
+
+const OrdersCharts: React.FC = () => {
+  const { data, error, isLoading, refetch } = useQueryOrders();
+
+  return (
+    <div className="flex flex-wrap gap-2">
+      <OrdersWorldMapChart
+        countriesQueryResult={{
+          countries: data?.map((item) => item.shipCountry),
+          error: error,
+          isLoading: isLoading,
+          refetch: refetch,
+        }}
+        hue={216}
+        allowZoom
+      />
+      <OrdersBarChart />
+    </div>
+  );
+};
+
+const SuppliersCharts: React.FC = () => {
+  const { data, error, isLoading, refetch } = useQuerySuppliers();
+
+  return (
+    <div className="flex flex-wrap gap-2">
+      <SuppliersWorldMapChart
+        countriesQueryResult={{
+          countries: data?.map((item) => item.country),
+          error: error,
+          isLoading: isLoading,
+          refetch: refetch,
+        }}
+        hue={120}
+        allowZoom
+      />
+      <SuppliersBarChart />
+    </div>
   );
 };
 
--- src/features/charts/index.tsx
+++ src/features/charts/index.tsx
@@ -1,2 +1,1 @@
 export { default as Charts } from './Charts';
-export { default as HeaderChart } from './HeaderChart';
