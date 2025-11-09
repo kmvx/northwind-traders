c298472c74ff4de28a3d92bb908aa210de39b4d8
Kolya Kosenko <kansev4k@gmail.com>
2025-11-10 00:44:13 +0200

    Add status API route

---
+++ src/app/api/status/route.ts
@@ -0,0 +1,35 @@
+import os from 'os';
+
+const start = new Date();
+
+export async function GET(request: Request) {
+  const now = new Date();
+  return Response.json({
+    app: {
+      startTime: start.toISOString(),
+      nowTime: now.toISOString(),
+      uptimeRouteSeconds: (now.getTime() - start.getTime()) / 1e3,
+      uptimeSeconds: process.uptime(),
+      name: process.env.npm_package_name,
+      version: process.env.npm_package_version,
+      NODE_ENV: process.env.NODE_ENV,
+    },
+    node: {
+      version: process.version,
+      platform: os.platform(),
+      arch: os.arch(),
+    },
+    memory: {
+      totalmem_Gbytes: os.totalmem() / 1e9,
+      freemem_Gbytes: os.freemem() / 1e9,
+      memoryUsage: process.memoryUsage(),
+    },
+    cpu: {
+      loadavg: os.loadavg(),
+      cpuUsage: process.cpuUsage(),
+      cpusCount: os.cpus().length,
+      cpus: os.cpus(),
+    },
+    headers: Object.fromEntries(request.headers.entries()),
+  });
+}
