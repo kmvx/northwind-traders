e065f9c28a0447c976fc7bd25da67d316212f3e1
Kolya Kosenko <kansev4k@gmail.com>
2025-12-11 05:37:14 +0200

    Add Skeletons to UserPreview component

--- src/features/auth/UserPreview.tsx
+++ src/features/auth/UserPreview.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-import { Button, Spinner } from '@/components/ui';
+import { Button, Skeleton } from '@/components/ui';
 import { useCloseSidebar } from '@/hooks';
 import { ErrorMessage } from '@/ui';
 
@@ -13,35 +13,49 @@ const UserPreview: React.FC = () => {
   const { data, isPending, error, refetch } = authClient.useSession();
   const user = data?.user;
 
-  const getNoUserContent = () => {
-    if (error) return <ErrorMessage error={error} retry={refetch} />;
-    if (isPending) return <Spinner className="size-9" />;
-    return <LoginButton />;
-  };
-
   if (!user) {
-    return <div className="flex justify-center">{getNoUserContent()}</div>;
+    if (error) return <ErrorMessage error={error} retry={refetch} />;
+    if (isPending) return <LocalSceleton />;
+    return (
+      <div className="flex justify-center">
+        <LoginButton />
+      </div>
+    );
   }
 
   return (
-    <Button
-      onClick={closeSidebar}
-      title={JSON.stringify(user, null, 4)}
-      asChild
-      variant="ghost"
-      className="ml-2 flex-grow p-0"
-    >
-      <Link href="/auth/user">
-        <UserAvatar user={user} />
-        <div className="grid flex-grow">
-          <span className="truncate text-sm font-bold">{user.name}</span>
-          <span className="text-muted-foreground truncate text-xs">
-            {user.email}
-          </span>
-        </div>
-      </Link>
-    </Button>
+    <div className="flex flex-col gap-2">
+      <Button
+        onClick={closeSidebar}
+        title={JSON.stringify(user, null, 4)}
+        asChild
+        variant="ghost"
+        className="ml-2 p-0"
+      >
+        <Link href="/auth/user">
+          <UserAvatar user={user} />
+          <div className="grid flex-grow">
+            <span className="truncate text-sm font-bold">{user.name}</span>
+            <span className="text-muted-foreground truncate text-xs">
+              {user.email}
+            </span>
+          </div>
+        </Link>
+      </Button>
+    </div>
   );
 };
+
+function LocalSceleton() {
+  return (
+    <div className="ml-2 flex w-full items-center gap-2">
+      <Skeleton className="size-8 rounded-md" />
+      <div className="grid flex-grow gap-2">
+        <Skeleton className="h-4 w-15" />
+        <Skeleton className="h-3 w-40" />
+      </div>
+    </div>
+  );
+}
 
 export default UserPreview;
