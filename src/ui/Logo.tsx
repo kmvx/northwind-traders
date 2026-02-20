4088a239776205775affe9ae5129d832c6d5db25
Kolya Kosenko <kolya.kosenko@gmail.com>
2026-02-20 11:33:54 +0200

    Simplify logo

--- src/ui/Logo.tsx
+++ src/ui/Logo.tsx
@@ -6,7 +6,7 @@ export default function Logo() {
       href="/"
       className="bg-gradient-to-r from-blue-600 to-teal-600 bg-clip-text font-serif text-4xl font-black text-transparent italic drop-shadow-lg dark:from-blue-300 dark:to-teal-300"
     >
-      Northwind Traders
+      Northwind
     </Link>
   );
 }
